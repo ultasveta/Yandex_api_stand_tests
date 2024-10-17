0079180631d2dd09a90cb282de025cb8f563c570
Svetlana Ultanbaeva <svetlana@MacBook-Air-Svetlana.local>
2024-10-17 18:58:08 +0300

    My first commit

---
+++ data.py
@@ -0,0 +1,11 @@
+headers={
+    "Content-Type": "application/json"
+}
+user_body={
+    "firstName": "Анатолий",
+    "phone": "+79995553322",
+    "address": "г. Москва, ул. Пушкина, д. 10"
+}
+products_ids = {
+     "ids":[1, 2, 3]
+ }
